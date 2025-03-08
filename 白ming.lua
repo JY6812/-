dc7395389c25aec347c385710e000d81a50532b7
JY6812 <3593096320@qq.com>
2025-03-08 13:29:58 +0800

    Add files via upload

--- 白ming.lua
+++ 白ming.lua
@@ -1,27 +1,27 @@
-当地的StarterGui=游戏：GetService("StarterGui")
-当地的玩家=游戏：GetService("玩家")
-当地的白名单玩家={
-["qazplm1005"] =正确,
-["玩家用户名"] =正确,
-["玩家用户名"] =正确,
-["玩家用户名"] =正确,
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local WhitelistedPlayers = {
+    ["qazplm10059"] = true,
+    ["玩家用户名"] = true,
+    ["玩家用户名"] = true,
+    ["玩家用户名"] = true,
 }
 
-当地的 功能列入白名单(玩家)
-    返回白名单玩家[player.name]或 假的
-结束
+local function IsWhitelisted(player)
+    return WhitelistedPlayers[player.Name] or false
+end
 
-当地的localPlayer=players.LocalPlayer
+local localPlayer = Players.LocalPlayer
 
-当地的isLocalPlayerWhitelisted=IsWhitelisted(localPlayer)
+local isLocalPlayerWhitelisted = IsWhitelisted(localPlayer)
 
-如果isLocalPlayerWhitelisted然后
-StarterGui:SetCore("发送通知", {
-title="白名单认证",
-文本="玩家:"..localPlayer.Name.."，检测完毕白名单玩家，稍后会加载脚本",
-持续时间=7,
-})
-loadstring(游戏：HttpGet("https://raw.githubusercontent.com/fifiopop/rr/refs/heads/main/666666%E8%84%9A%E6%9C%AC.lua"))()
-其他
-localPlayer：踢("你没有被加入白名单，请找群主反馈或购买")
-结束
+if isLocalPlayerWhitelisted then
+    StarterGui:SetCore("SendNotification", {
+        Title = "白名单认证",
+        Text = "玩家:"..localPlayer.Name.."，检测完毕白名单玩家，稍后会加载脚本",
+        Duration = 7,
+    })
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fifiopop/rr/refs/heads/main/666666%E8%84%9A%E6%9C%AC.lua"))()
+else
+    localPlayer:Kick("你没有被加入白名单，请找群主反馈或购买")
+end
